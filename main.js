1c84679f0101f8cba1b87b535c012af43a25c9d8
Lauren Hoock <laurenh8916@protonmail.com>
2022-05-15 07:18:08 -0600

    started implementing server code

--- main.js
+++ main.js
@@ -1,102 +1,158 @@
 // Execution Flow
-//----------------
-// 1 - getSomePokemon calls rng for a given quantity of random nums
-// 1a - getSomePokemon calls fetchEm to fetch pokemon data
-// 1b - fetchEm calls createCard on each pokemon object retrieved and appends to dom
-
-// But what if...
+//----------------//
 // Generate random pokemon
 // each pokemon has a catch button
 // catch button randomly decides if your throw is successful
 // if successful the pokemon is added to your collection
 // if not, new pokemon is generated
 
-getSomePokemon(6);
+getAPokemon();
 
-function getSomePokemon(quantity) {
-  const pokeNums = randomNumGen(1, 151, quantity);
-  fetchEm(pokeNums);
+function getAPokemon() {
+  fetchEm(randomNumGen(1, 151));
 }
 
-function randomNumGen(min, max, quantity) {
-  // Returns an array of the specified quantity of random numbers
-  const randomArray = [];
+function clearWildPokemon() {
+  const currentPokemon = document.querySelector("#wildPokemonContainer");
+  currentPokemon.removeChild(currentPokemon.firstChild);
+}
 
-  for (let i = 0; i < quantity; i++) {
-    randomArray.push(Math.floor(Math.random() * (max - min) + min));
+function capitalizeName(name) {
+  return name[0].toUpperCase() + name.slice(1);
+}
+
+function randomNumGen(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function fetchEm(randomNum) {
+  // Fetch a pokemon from passed in random num and call createCard() on it
+  fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
+    .then((response) => response.json())
+    .then((data) => {
+      createPokemonCard(data, "wildPokemon");
+      sendDataToServer(data, "wildPokemon");
+    }
+      );
+}
+
+function createPokemonCard(pokemonObj, typeOfCard) {
+  function cardContainer() {
+    let newCard = document.createElement("div");
+    newCard.className = "card";
+    return newCard;
   }
 
-  return randomArray;
+  function cardHeader() {
+    let pokeHeader = document.createElement("h3");
+    pokeHeader.className = "card-header";
+
+    switch (typeOfCard) {
+      case "wildPokemon":
+        pokeHeader.textContent = `A wild ${capitalizeName(pokemonObj.name)} has appeared!`;
+        break;
+      case "caughtPokemon":
+        pokeHeader.textContent = capitalizeName(pokemonObj.name);
+        break;
+    }
+    return pokeHeader;
+  }
+
+  function cardPic() {
+    let pokePic = document.createElement("img");
+    pokePic.className = "card-img-top";
+    pokePic.src = `${pokemonObj.sprites.other["official-artwork"].front_default}`;
+    return pokePic;
+  }
+
+  function cardBody() {
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    return cardBody;
+  }
+
+  function catchBtn() {
+    let catchBtn = document.createElement("button");
+    catchBtn.className = "btn btn-primary";
+    catchBtn.textContent = "Throw a Pokeball!";
+    catchBtn.addEventListener("click", sendDataToServer);
+    return catchBtn;
+  }
+
+  function runBtn() {
+    let runAwayBtn = document.createElement("button");
+    runAwayBtn.className = "btn btn-primary";
+    runAwayBtn.id = "runAway";
+    runAwayBtn.textContent = "Run Away!";
+    runAwayBtn.addEventListener("click", () => {
+      deleteDataFromServer();
+      clearWildPokemon();
+      getAPokemon();
+    });
+    return runAwayBtn;
+  }
+
+  const newCardContainer = cardContainer();
+  const newCardHeader = cardHeader();
+  const newCardPic = cardPic();
+  const newCardBody = cardBody();
+  const newCatchBtn = catchBtn();
+  const newRunBtn = runBtn();
+
+  switch (typeOfCard) {
+    case "wildPokemon":
+      const wildPokemonContainer = document.querySelector(
+        "#wildPokemonContainer"
+      );
+
+      newCardBody.appendChild(newCatchBtn);
+      newCardBody.appendChild(newRunBtn);
+      newCardContainer.appendChild(newCardHeader);
+      newCardContainer.appendChild(newCardPic);
+      newCardContainer.appendChild(newCardBody);
+      wildPokemonContainer.appendChild(newCardContainer);
+      break;
+
+    case "caughtPokemon":
+      const caughtPokemonContainer = document.querySelector("#caughtPokemonContainer");
+      newCardContainer.appendChild(newCardHeader);
+      newCardContainer.appendChild(newCardPic);
+      caughtPokemonContainer.appendChild(newCardContainer);
+      break;
+  }
 }
 
-function fetchEm(numArray) {
-  // Fetch each pokemon from numArray and call createCard() on it
-  numArray.forEach((num) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)
-      .then((response) => response.json())
-      .then((data) => createCard(data));
-  });
+function convertPokeObj(pokeObject) {
+  // Saves relevant data retrieved from API
+  let newPokemon = {
+    id: pokeObject.order,
+    name: pokeObject.name,
+  }
+
+  return newPokemon
 }
 
-function createCard(pokemonObj) {
-  // create card container
-  let newCard = document.createElement("div");
-  newCard.className = "card";
+function sendDataToServer(pokeObject, whereToSend) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(convertPokeObj(pokeObject)),
+  };
 
-  // add name
-  let pokeName = document.createElement("h3");
-  pokeName.className = "card-header";
-  pokeName.textContent = pokemonObj.name;
-  newCard.appendChild(pokeName);
-
-  // add image
-  let pokePic = document.createElement("img");
-  pokePic.className = "card-img-top";
-  pokePic.src = `${pokemonObj.sprites.other["official-artwork"].front_default}`;
-  newCard.appendChild(pokePic);
-
-  // create card body
-  let cardBody = document.createElement("div");
-  cardBody.className = "card-body";
-  newCard.appendChild(cardBody);
-
-  // add number
-  let pokeNum = document.createElement("p");
-  pokeNum.className = "card-subtitle";
-  pokeNum.textContent = `#${pokemonObj.order}`;
-  cardBody.appendChild(pokeNum);
-
-  // add card text
-  let cardText = document.createElement("p");
-  cardText.className = "card-text";
-  cardText.textContent = "lorum ipsum de facto oh hai";
-  cardBody.appendChild(cardText);
-
-  // create list container
-  let statsContainer = document.createElement("ul");
-  statsContainer.className = "list-group list-group-flush";
-  newCard.appendChild(statsContainer);
-
-  // add type
-  let pokeType = document.createElement("li");
-  pokeType.className = "list-group-item";
-  let typeArray = [];
-  pokemonObj.types.forEach((obj) => typeArray.push(obj.type.name));
-  pokeType.textContent = `${typeArray.join(" / ")}`;
-  statsContainer.appendChild(pokeType);
-
-  // add height
-  let pokeHeight = document.createElement("p");
-  pokeHeight.className = "list-group-item";
-  pokeHeight.textContent = `Height: ${pokemonObj.height} decimeters`;
-  statsContainer.appendChild(pokeHeight);
-
-  // add weight
-  let pokeWeight = document.createElement("p");
-  pokeWeight.className = "list-group-item";
-  pokeWeight.textContent = `Weight: ${pokemonObj.weight} hectograms`;
-  statsContainer.appendChild(pokeWeight);
-
-  document.querySelector("#pokeball").appendChild(newCard);
+  fetch(`http://localhost:3000/${whereToSend}`, configurationObject);
 }
 
+function deleteDataFromServer() {
+  const configurationObject = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+  };
+
+  fetch(`http://localhost:3000/wildPokemon/88`, configurationObject);
+}
